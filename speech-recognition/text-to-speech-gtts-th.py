01f9fe2f913e7090c6fb2de25d55b0f195dcf68d
Noppanut Thongton <nptttn.pat@gmail.com>
2022-11-29 20:45:55 -0800

    Delete previous package

--- speech-recognition/text-to-speech-gtts-th.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-
-mytext = 'สวัสดีครับ'
-
-language = 'th'
-
-myobj = gTTS(text=mytext, lang=language, slow=False)
-
-myobj.save("welcome.mp3")
--- speech-recognition/text-to-speech-gtts.py
+++
@@ -1,9 +0,0 @@
-from gtts import gTTS
-
-mytext = 'Welcome to AI for robotics!'
-
-language = 'en'
-
-myobj = gTTS(text=mytext, lang=language, slow=False)
-
-myobj.save("welcome.mp3")
